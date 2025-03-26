5d268a166b9c221bbf3a7ec4b28a2be8c2dd17c5
Kutullo Letageng <u21585513@tuks.co.za>
2025-03-26 21:36:05 +0200

    Added variables to store references to all HTML task manager UI elements including views, lists, and buttons.

---
+++ app.js
@@ -0,0 +1,17 @@
+const views = {
+    taskList: document.getElementById('taskListView'),
+    addTask: document.getElementById('addTaskView'),
+    completedTasks: document.getElementById('completedTasksView')
+};
+const lists = {
+    tasks: document.getElementById('tasksList'),
+    completed: document.getElementById('completedTasksList')
+};
+const buttons = {
+    taskList: document.getElementById('taskListBtn'),
+    addTask: document.getElementById('addTaskBtn'),
+    completedTasks: document.getElementById('completedTasksBtn')
+};
+const addTaskForm = document.getElementById('addTaskForm');
+const mobileMenuBtn = document.getElementById('mobileMenuBtn');
+const headerLeft = document.getElementById('headerLeft');
